b38d7701c8a2f0fbae48bfb06fd2a93e9209c9c1
GregoireCorluy <gcorluy@gmail.com>
2023-03-30 00:45:42 +0200

    first part basics visualization

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -1,21 +1,40 @@
 <script>
-    const width = 100;
-    const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  </script>
+  const width = 100;
+  const height = 100;
+  const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const size_rect = 40;
+  const size_circ = 20;
+</script>
+
+<svg {width} {height}>
+  <g transform="translate({margin.left}, {margin.top})">
+    <!--  -->
+    <rect x={(innerWidth-size_rect)/2} y={(innerHeight-size_rect)/2} width={size_rect} height={size_rect}/>
+    
   
-  <svg {width} {height}>
-    <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
-    </g>
-  </svg>
+  </g>
   
-  <style>
-    svg {
-      border: 1px solid black;
-      border-radius: 5px;
-    }
-  </style>
+  <!-- don't know if it has to be inside the group-->
+  <circle cx={width-size_circ} cy={height-size_circ} r={size_circ} />
+  
+</svg>
+
+<style>
+  svg {
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  rect{
+    fill: yellow;
+  }
+
+  circle{
+    fill: skyblue;
+  }
+</style>
+
   
--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -1,18 +1,32 @@
 <script>
-    // Dimensions
-    const width = 800;
-    const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  
-    // Array
-    const values = [2, 4, 6, 7, 9];
-  </script>
-  
-  <svg viewBox="0 0 {width} {height}">
-    <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
-    </g>
-  </svg>
-  
+  // Dimensions
+  const width = 800;
+  const height = 100;
+  const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const radius = 10;
+  const radiusSmall = 2;
+
+  // Array
+  const values = [2, 4, 6, 7, 9];
+
+  import {scaleLog} from "d3-scale";
+
+  const myScale = scaleLog().domain([values.sort()[0],values.sort()[values.length-1]]).range([radius,innerWidth-radius]);
+</script>
+
+
+
+<svg viewBox="0 0 {width} {height}">
+  <g transform="translate({margin.left},{margin.top})">
+    <!--  -->
+    {#each values as val}
+      <circle style="fill:red;" cx={myScale(val)} cy = {innerHeight/2} r = {radius}/>
+      <circle style="fill:green;" cx={myScale(val)} cy = {innerHeight/2} r = {radiusSmall}/>
+      
+      
+      {/each}
+  </g>
+</svg>
