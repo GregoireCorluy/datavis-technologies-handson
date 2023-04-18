da5c68ad0c66e4a642ca889769348c45d1c2880c
GregoireCorluy <gcorluy@gmail.com>
2023-04-18 09:37:15 +0200

    exercise 22 done buttons

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,31 @@
 <script>
+
+    let counter = 0;
+
+    function Increase(event) {
+        counter++;
+    console.log("Clicked Increase!");
+    }
     
+    function Reset(event) {
+        counter = 0;
+        console.log("Clicked Reset!");
+    }
 </script>
+
+Number of times clicked: {counter}.
+<br/>
+<button on:click={Increase}>Increase</button>
+<br/>
+<button on:click={Reset}>Reset</button>
+
+<style>
+    button{
+        border-radius:6px;
+        background-color: red;
+        color:rgb(239, 231, 5);
+        font-weight:bold;
+        margin-top: 0.3em;
+        margin-bottom: 0.3em;
+    }
+</style>
