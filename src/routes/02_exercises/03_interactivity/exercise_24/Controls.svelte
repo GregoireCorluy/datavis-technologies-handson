32b7a1ce5b94b6b40217e644e5eb70151a1559d9
GregoireCorluy <gcorluy@gmail.com>
2023-04-21 12:10:56 +0200

    exercise 24 done, without fancy styling

--- src/routes/02_exercises/03_interactivity/exercise_24/Controls.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_24/Controls.svelte
@@ -1,25 +1,56 @@
 <script>
+  import {onMount} from 'svelte';
+
   // Properties
   export let data = [];
   const continents = ["europe", "asia", "americas", "africa"];
 
-  let play = false;
-  $: text_button = ((play) ? "Play" : "Pause");
+  let play = true;
+  let increase_delta = 1;
+  $: text_button = ((play) ? "Pause" : "Play");
+  $: increase = ((play) ? increase_delta : 0);
+
+  let reset_variable = false;
   //let text_button = "Pause";
 
   const year_base = 1800;
-  let value_year = 0;
-  let selected_continent;
+  const max_year_increase = 214;
+  export let value_year = 0;
+  export let selected_continent;
+  //let increase=1;
 
   function play_pause(){
 
     play = !play;
+
+    /*if(play){
+      increase = 1;
+    }
+    else if(!play){
+      increase = 0;
+    }*/
   }
 
-  function reset_function(){
-
-
+  function reset_button(){
+    play = false;
+    value_year = 0;
+    //increase = 0;
   }
+
+  let my_interval;
+
+    onMount(() => {
+      my_interval = setInterval(update_slider, 100);
+    });
+
+  function update_slider(){
+    value_year = value_year+increase;
+    
+    if(value_year>max_year_increase){
+      value_year=0;
+    }
+  }
+  
 
 </script>
 
@@ -28,12 +59,12 @@
   <button on:click={play_pause} >
     {text_button}
   </button>
-  <br/>
-  <button on:click={reset_function} >
+  
+  <button on:click={reset_button} >
     Reset
   </button>
   <br/>
-  <input type=range bind:value={value_year} min=0 max=100>
+  <input type=range bind:value={value_year} min=0 max={max_year_increase}>
   <label>Year: {year_base+value_year}</label>
   <br/>
   <select bind:value={selected_continent} on:change>
@@ -44,6 +75,7 @@
 		{/each}
 	</select>
   </div>
+ 
 
 <style>
 button{
--- src/routes/02_exercises/03_interactivity/exercise_24/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_24/Exercise.svelte
@@ -5,18 +5,27 @@
   import Controls from "./Controls.svelte";
   import Scatterplot from "./Scatterplot.svelte";
 
+  
+  let selected_continent_ex='europe';
+  let selected_year=0;
+
+  
   // Load the data
   let data = null;
   onMount(async () => {
     data = await json("/data/gapminder.json");
+    
   });
+
+  $: selected_data = data ? data[selected_year]['countries'].filter(e => e["continent"]==selected_continent_ex):[];
+
 </script>
 
 {#if !data}
   <p>Loading the data, please wait...</p>
 {:else}
   <div>
-    <Scatterplot data={data[0]['countries']} />
-    <Controls {data} />
+    <Scatterplot data={selected_data} />
+    <Controls bind:selected_continent={selected_continent_ex} bind:value_year={selected_year} {data} />
   </div>
 {/if}
